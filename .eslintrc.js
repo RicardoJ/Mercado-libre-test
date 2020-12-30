edae781e61e869095508147ec26846526c0d51f2
RicardoJ <rjdiazzh@gmail.com>
2020-12-29 21:29:47 -0500

    initializing base project

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parserOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    parser: 'babel-eslint',
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  }
+  
---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+ 
+  return (
+    <div >
+     ML
+    </div>
+  );
+}
+
+export default App;
