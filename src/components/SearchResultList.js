b5ee42b75eee87ee21cb54a36a1d2dc75eac9310
RicardoJ <rjdiazzh@gmail.com>
2020-12-31 17:44:26 -0500

    adding styles and show title , proce , photo of the actual product

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import logoMercadoLibre from '../assets/images/Logo_ML@2x.png.png';
 import shipping from '../assets/images/ic_shipping.png';
 import { ProductContext } from '../Context';
 import { TOTAL_LISTED_PRODUCTS } from '../constants';
@@ -9,18 +8,18 @@ const SearchResultList = () => {
   const filteredProducts = products.filter(
     (_, index) => index < TOTAL_LISTED_PRODUCTS,
   );
-console.log(filteredProducts);
+
   return (
     <>
-      {filteredProducts.map((product, index) => (
-        <li key={index} className='ui-search-layout__item'>
+      {filteredProducts.map(product => (
+        <li key={product.id} className='ui-search-layout__item'>
           <div className='ui-search-result__image'>
             <a href='#' title='titulo get endpoint'>
               <img
                 className='ui-search-result-image__element'
                 width='180'
                 height='180'
-                src={logoMercadoLibre}
+                src={product.thumbnail}
                 alt='logo mercado libre'
               />
             </a>
@@ -28,17 +27,17 @@ console.log(filteredProducts);
           <div className='ui-search-result__content'>
             <div className='ui-search-item__group'>
               <a href='#' className='ui-search-link'>
-                $1.908
+                ${product.price}
               </a>
               <img src={shipping} alt='shipping' />
             </div>
             <div className='ui-search-item__group--title'>
-              <a href='#'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </a>
+              <a href='#'>{product.title}</a>
             </div>
           </div>
-          <div className='ui-search-result__location'>ciudad</div>
+          <div className='ui-search-result__location'>
+            {product.address.state_name}
+          </div>
         </li>
       ))}
     </>
